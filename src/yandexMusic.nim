a07c29e868c97c67cd2ad8b1c5a75409a3efc816
levovix0 <xlevovix@gmail.com>
2021-10-22 01:04:09 +0300

    add: Больше функций в api-wrapper яндекс.музыки на nim

--- src/yandexMusic.nim
+++ src/yandexMusic.nim
@@ -1,4 +1,4 @@
-import httpclient, strformat, asyncdispatch, json, uri, xmltree, xmlparser, md5
+import httpclient, strformat, strutils, sequtils, asyncdispatch, json, uri, xmltree, xmlparser, md5
 
 type
   HttpError* = object of CatchableError
@@ -11,6 +11,7 @@ type
     httpc: AsyncHttpClient
 
   TrackId* = object
+    #TODO: user-added tracks
     id*: int
     # album*: int
 
@@ -40,27 +41,40 @@ type
     coverUri*: string
     year*: int
     len*: int
+  
+  Playlist* = object
+    id*: int
+    kind*: int
+    ownerId*: int
+
+    title*: string
+    description*: string
+    coverUri*: string
+    duration*: int ## in milliseconds
+    len*: int
+  
+  Account* = object
+    id*: int
+    
+    name*: string
 
 
-proc `lang=`*(this: Client, lang: string) =
-  this.headers["Accept-Language"] = lang
+proc `lang=`*(client: Client, lang: string) =
+  client.headers["Accept-Language"] = lang
 
-proc `token=`*(this: Client, token: string) =
-  this.headers["Authorization"] = &"OAuth {token}"
+proc `token=`*(client: Client, token: string) =
+  client.headers["Authorization"] = if token != "": &"OAuth {token}" else: ""
 
-proc newClient*(lang="ru"): Client =
+proc newClient*(token="", lang="ru"): Client =
   new result
   result.httpc = newAsyncHttpClient()
   result.headers = newHttpHeaders()
-  result.lang = lang
   result.headers["User-Agent"] = "Yandex-Music-API"
-
-proc newClient*(token: string): Client =
-  result = newClient()
   result.token = token
+  result.lang = lang
 
 proc request*(
-  this: Client,
+  client: Client,
   url: string,
   httpMethod: HttpMethod = HttpGet,
   body = "",
@@ -72,13 +86,27 @@ proc request*(
     if params.len == 0: url
     else: $(url.parseUri ? params)
 
-  let response = await this.httpc.request(url, httpMethod, body, this.headers, data)
+  let response = await client.httpc.request(url, httpMethod, body, client.headers, data)
+
+  template formatResponse: string =
+    let body = response.body.await.parseJson
+    if body{"error", "message"} != nil:
+      response.status & ", " & body["error"]["message"].getStr
+    else:
+      response.status
+
   case response.code
   of Http200..Http206: return await response.body
-  of Http400: raise BadRequestError.newException(response.status)
-  of Http401, Http403: raise UnauthorizedError.newException(response.status)
-  of Http502: raise BadGatewayError.newException(response.status)
-  else: raise HttpError.newException(response.status)
+  of Http400: raise BadRequestError.newException(formatResponse)
+  of Http401, Http403: raise UnauthorizedError.newException(formatResponse)
+  of Http502: raise BadGatewayError.newException(formatResponse)
+  else: raise HttpError.newException(formatResponse)
+
+
+iterator items(a: JsonNode): JsonNode =
+  if a != nil and a.kind == JArray:
+    for x in json.items(a):
+      yield x
 
 
 const
@@ -86,22 +114,31 @@ const
   oauthUrl = "https://oauth.yandex.ru"
 
 
+proc toInt(a: JsonNode): int =
+  if a.kind == JString:
+    try: a.getStr.parseInt except: 0
+  else: a.getInt
+
+
 proc parseArtist*(a: JsonNode): Artist =
-  result.id = a{"id"}.getInt
+  result.id = a{"id"}.toInt
 
   result.name = a{"name"}.getStr
   result.coverUri = a{"cover", "uri"}.getStr
 
 proc parseAlbum*(a: JsonNode): Album =
-  result.id = a{"id"}.getInt
+  result.id = a{"id"}.toInt
 
   result.title = a{"title"}.getStr
   result.coverUri = a{"coverUri"}.getStr
   result.year = a{"year"}.getInt
   result.len = a{"trackCount"}.getInt
 
+proc parseTrackId*(a: JsonNode): TrackId =
+  result.id = a{"id"}.toInt
+
 proc parseTrack*(a: JsonNode): Track =
-  result.id = a{"id"}.getInt
+  result.id = a{"id"}.toInt
 
   result.title = a{"title"}.getStr
   result.comment = a{"version"}.getStr
@@ -115,10 +152,26 @@ proc parseTrack*(a: JsonNode): Track =
   
   for artist in a{"artists"}:
     result.artists.add artist.parseArtist
+  
+proc parsePlaylist*(a: JsonNode): Playlist =
+  result.id = a{"uid"}.toInt
+  result.kind = a{"kind"}.getInt
+  result.ownerId = a{"owner", "uid"}.getInt
+
+  result.title = a{"title"}.getStr
+  result.description = a{"description"}.getStr
+  result.coverUri = a{"cover", "uri"}.getStr
+  result.duration = a{"durationMs"}.getInt
+  result.len = a{"trackCount"}.getInt
+  
+proc parseAccount*(a: JsonNode): Account =
+  result.id = a{"uid"}.toInt
+
+  result.name = a{"displayName"}.getStr
 
 
-proc generateToken*(this: Client, username, password: string): Future[string] {.async.} =
-  return this.request(
+proc generateToken*(client: Client, username, password: string): Future[string] {.async.} =
+  return client.request(
     &"{oauthUrl}/token", HttpPost, data = newMultipartData {
       "grant_type": "password",
       "client_id": "23cabbbdc6cd418abb4b39c32c41195d",
@@ -128,10 +181,10 @@ proc generateToken*(this: Client, username, password: string): Future[string] {.
     }
   ).await.parseJson["access_token"].getStr
 
-proc search*(this: Client, text: string, kind = "all", correct = true): Future[tuple[tracks: seq[Track]]] {.async.} =
+proc search*(client: Client, text: string, kind = "all", correct = true): Future[tuple[tracks: seq[Track]]] {.async.} =
   if text == "": return
 
-  let response = this.request(
+  let response = client.request(
     &"{baseUrl}/search", params = @{
       "text": text,
       "nocorrect": $(not correct),
@@ -141,43 +194,44 @@ proc search*(this: Client, text: string, kind = "all", correct = true): Future[t
     }
   ).await.parseJson
 
-  if response{"result", "tracks", "results"} != nil:
-    for track in response{"result", "tracks", "results"}:
-      result.tracks.add track.parseTrack
+  for track in response{"result", "tracks", "results"}:
+    result.tracks.add track.parseTrack
   #TODO: albums and artists
 
 
-proc coverUrl*(this: Track|Album|Artist, size = 1000): string =
+proc coverUrl*(client: Track|Album|Artist|Playlist, size = 1000): string =
   ## direct link to track's cover
-  "https://" & this.coverUri.replace("%%", &"{size}x{size}")
+  "https://" & client.coverUri.replace("%%", &"{size}x{size}")
 
 
-proc `$`*(this: TrackId): string = $this.id
+proc `$`*(x: TrackId): string = $x.id
+
+converter asId*(x: Track): TrackId = TrackId(id: x.id)
 
 
-proc fetch*(this: Client, ids: TrackId|seq[TrackId], withPositions = true): Future[seq[Track]] {.async.} =
+proc fetch*(ids: TrackId|seq[TrackId], client: Client, withPositions = true): Future[seq[Track]] {.async.} =
   ## get full track information by id
   when ids is seq[TrackId]:
     if ids.len < 1: return
   
-  let response = this.request(
+  let response = client.request(
     &"{baseUrl}/tracks", HttpPost, data = newMultipartData {
-      "track-ids": $ids,
+      "track-ids": if ids is TrackId: $ids else: ids.join(","),
       "with-positions": $withPositions
     }
   ).await.parseJson
 
-  for res in response:
+  for res in response{"result"}:
     result.add res.parseTrack
 
-proc audioUrl*(this: Client, track: Track|TrackId): Future[string] {.async.} =
+proc audioUrl*(track: Track|TrackId, client: Client): Future[string] {.async.} =
   ## get direct link to track's audio
-  ##! works only one minute after call!
-  let infoUrl = this.request(
+  ## ! works only one minute after call
+  let infoUrl = client.request(
     &"{baseUrl}/tracks/{track.id}/download-info"
   ).await.parseJson["downloadInfoUrl"].getString
     
-  let result = await this.request(infoUrl).parseXml
+  let result = await client.request(infoUrl).parseXml
   let host = result.findAll("host")[0].text
   let path = result.findAll("path")[0].text
   let ts = result.findAll("ts")[0].text
@@ -186,3 +240,50 @@ proc audioUrl*(this: Client, track: Track|TrackId): Future[string] {.async.} =
   
   return &"https://{host}/get-mp3/{sign}/{ts}{path}"
 
+
+proc currentUser*(client: Client): Future[Account] {.async.} =
+  let response = client.request(
+    &"{baseUrl}/account/status"
+  ).await.parseJson
+  return response{"result", "account"}.parseAccount
+
+
+proc likedTracks*(user: Account, client: Client): Future[seq[TrackId]] {.async.} =
+  ## get smart playlists for user
+  let response = client.request(
+    &"{baseUrl}/users/{user.id}/likes/tracks"
+  ).await.parseJson
+
+  for track in response{"result", "library", "tracks"}:
+    result.add track.parseTrackId
+  
+  result = result.filterit(it.id != 0)
+
+
+proc landing*(client: Client, blocks: string): Future[JsonNode] {.async.} =
+  ## get block
+  ## ? requires token
+  #TODO: seq of blocks
+  return client.request(
+    &"{baseUrl}/landing3", params = @{
+      "blocks": blocks,
+      "eitherUserId": "10254713668400548221"
+    }
+  ).await.parseJson
+
+proc personalPlaylists*(client: Client): Future[seq[tuple[kind: string, playlist: Playlist]]] {.async.} =
+  ## get smart playlists for current user
+  ## * requires token
+  let response = await client.landing("personalplaylists")
+  for Block in response{"result", "blocks"}:
+    for entity in Block{"entities"}:
+      result.add (entity{"data"}{"type"}.getStr, entity{"data"}{"data"}.parsePlaylist)
+
+
+proc tracks*(playlist: Playlist, client: Client): Future[seq[Track]] {.async.} =
+  let response = client.request(
+    &"{baseUrl}/users/{playlist.ownerId}/playlists/{playlist.kind}"
+  ).await.parseJson
+
+  for track in response{"result", "tracks"}:
+    result.add track{"track"}.parseTrack
